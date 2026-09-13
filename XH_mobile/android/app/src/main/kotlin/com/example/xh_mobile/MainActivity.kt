cd5623775d86adcf0c5abf04b747676428000051
Xenon Akro <metrinaveen08@gmail.com>
2026-09-13 15:36:04 +0530

    Delete XH_mobile directory

--- XH_mobile/android/app/src/main/kotlin/com/example/xh_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.xh_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- XH_mobile/android/app/src/main/kotlin/com/xenhide/xh_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.xenhide.xh_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
